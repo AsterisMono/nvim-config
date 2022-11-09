34a33a7949ed8ba220576eb2bf28fb9c5da8266e
AsterisMono <cmiki@amono.me>
2022-11-09 16:31:50 +0800

    Add keymaps

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ vim.g.loaded_netrwPlugin = 1
 
 require('asterismono.base')
 require('asterismono.plugins')
+require('asterismono.maps')
---
+++ lua/asterismono/maps.lua
@@ -0,0 +1,7 @@
+local keymap = vim.keymap
+
+keymap.set('n', 'dw', 'diw')
+keymap.set('n', 'cw', 'ciw')
+
+keymap.set('n', 'te', ':tabedit')
+keymap.set('n', '<Space>', '<C-W>w')
