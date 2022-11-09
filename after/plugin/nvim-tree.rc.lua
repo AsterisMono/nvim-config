4a04a572627eaa00c13aab80570bd0a0c9daa838
AsterisMono <cmiki@amono.me>
2022-11-09 15:36:04 +0800

    Set nvim-tree to close on file open

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -1,5 +1,10 @@
 require('nvim-tree').setup({
   hijack_cursor = true,
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    }
+  }
 })
 
 vim.keymap.set('n', '\'\'', '<cmd>NvimTreeToggle<CR>', {})
