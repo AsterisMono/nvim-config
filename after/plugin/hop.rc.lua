bd8f8eb959a4aca59bbc6ecd2481a9211cf46565
AsterisMono <cmiki@amono.me>
2022-11-09 14:46:47 +0800

    Config treesitter

--- after/plugin/hop.rc.lua
+++ after/plugin/hop.rc.lua
@@ -2,4 +2,4 @@ local hop = require('hop')
 hop.setup()
 
 -- Triple leader key smash
-vim.keymap.set('', '<leader><leader><leader>', '<cmd>HopWord<CR>')
+vim.keymap.set('', '<leader><leader>', '<cmd>HopWord<CR>')
---
+++ after/plugin/nvim-treesitter.rc.lua
@@ -0,0 +1,33 @@
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "tsx",
+    "json",
+    "css",
+    "html",
+    "lua",
+    "yaml",
+    "javascript",
+    "typescript"
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      node_incremental = '<CR>',
+      node_decremental = '<BS>',
+    }
+  }
+}
+
+-- Folding by treesitter
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+-- No default folding
+vim.wo.foldlevel = 99
