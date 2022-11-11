ab82ea204656725d241afc5b1c7eeef06651f881
AsterisMono <cmiki@amono.me>
2022-11-11 16:17:11 +0800

    Add keymaps for sessions and liveserver

--- lua/asterismono/maps.lua
+++ lua/asterismono/maps.lua
@@ -1,13 +1,20 @@
 local keymap = vim.keymap
 
+-- Change word under cursor
 keymap.set('n', 'dw', 'diw')
 keymap.set('n', 'cw', 'ciw')
 
+-- Multi tab and window editing
 keymap.set('n', 'te', ':tabedit')
 keymap.set('n', '<Space>', '<C-w>w')
 
-keymap.set('n', '<leader>s', '<cmd>write<CR>')
+-- Save session
+keymap.set('n', '<leader>ss', '<cmd>mksession session.vim<CR>')
 
+-- Utility
 keymap.set('n', '<leader>h', '<cmd>noh<CR>')
 
-keymap.set('n', '<leader>c', '<cmd>ConvertColorTo rgba<CR>')
+-- Plugins
+keymap.set('n', '<leader>cr', '<cmd>ConvertColorTo rgba<CR>')
+keymap.set('n', '<leader>ch', '<cmd>ConvertColorTo hex<CR>')
+keymap.set('n', '<leader>ls', '<cmd>LiveServer start<CR>')
