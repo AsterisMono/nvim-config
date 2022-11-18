d5c3a9d14f3946cb47045b094868d8cb22558620
AsterisMono <cmiki@amono.me>
2022-11-18 12:39:27 +0800

    Add coc framework

--- lua/asterismono/plugins.lua
+++ lua/asterismono/plugins.lua
@@ -10,10 +10,13 @@ require('packer').startup(function(use)
   -- use 'glepnir/lspsaga.nvim' -- LSP UIs
   -- use 'jose-elias-alvarez/null-ls.nvim' -- Inject LSP diagnostics
   -- use 'ray-x/lsp_signature.nvim' -- Function signature and document hover
-  -- use {
-  --   'nvim-treesitter/nvim-treesitter',
-  --   run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  -- } -- Highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    config = function()
+      require('asterismono.plugin-rc.nvim-treesitter-rc')
+    end
+  } -- Highlighting
 
   -- Auto completion
   -- use 'hrsh7th/nvim-cmp'
@@ -24,41 +27,41 @@ require('packer').startup(function(use)
   -- use 'aca/emmet-ls' -- Smart HTML completion
 
   -- Coc framework
-
-
+  use { 'neoclide/coc.nvim', branch = 'release' }
 
   -- Finger efficiency
   use 'tpope/vim-surround'
-  use { 'justinmk/vim-sneak', config = require('asterismono.plugin-rc.vim-sneak-rc') }
+  use { 'justinmk/vim-sneak', config = function() require('asterismono.plugin-rc.vim-sneak-rc') end }
   use 'vim-scripts/ReplaceWithRegister'
   use 'tpope/vim-commentary'
-  use { 'phaazon/hop.nvim', config = require('asterismono.plugin-rc.gitsigns-rc') }
+  use { 'phaazon/hop.nvim', config = function() require('asterismono.plugin-rc.hop-rc') end }
   use 'wellle/targets.vim'
-  use { 'chentoast/marks.nvim', config = require('asterismono.plugin-rc.marks-nvim-rc') }
-  -- use { 'mizlan/iswap.nvim', config = require('asterismono.plugin-rc.iswap-rc') }
+  use { 'chentoast/marks.nvim', config = function() require('asterismono.plugin-rc.marks-nvim-rc') end }
+  use { 'mizlan/iswap.nvim', config = function() require('asterismono.plugin-rc.iswap-rc') end }
 
   -- Smarter editor
-  use { 'windwp/nvim-autopairs', config = require('asterismono.plugin-rc.nvim-autopairs') } -- Auto pair brackets
+  use { 'windwp/nvim-autopairs', config = function() require('asterismono.plugin-rc.nvim-autopairs') end } -- Auto pair brackets
   use 'jghauser/mkdir.nvim' -- Auto mkdir
-  use { 'AckslD/nvim-neoclip.lua', config = require('asterismono.plugin-rc.nvim-neoclip-rc') } -- Smart clipboard
+  use { 'AckslD/nvim-neoclip.lua', config = function() require('asterismono.plugin-rc.nvim-neoclip-rc') end } -- Smart clipboard
   use 'mg979/vim-visual-multi' -- Multi cursor
   use 'amadeus/vim-convert-color-to' -- Color code converter
-  use { 'yamatsum/nvim-cursorline', config = require('asterismono.plugin-rc.nvim-cursorline-rc') } -- Word underline
-  use { 'linty-org/key-menu.nvim', config = require('asterismono.plugin-rc.key-menu-rc') } -- Key suggestion
+  use { 'yamatsum/nvim-cursorline', config = function() require('asterismono.plugin-rc.nvim-cursorline-rc') end } -- Word underline
+  use { 'linty-org/key-menu.nvim', config = function() require('asterismono.plugin-rc.key-menu-rc') end } -- Key suggestion
 
   -- Telescope
-  use { 'nvim-telescope/telescope.nvim', config = require('asterismono.plugin-rc.telescope-rc') }
+  use { 'nvim-telescope/telescope.nvim', config = function() require('asterismono.plugin-rc.telescope-rc') end }
 
   -- UI components
-  use { 'akinsho/nvim-bufferline.lua', config = require('asterismono.plugin-rc.bufferline-rc') } -- Tabline
-  use { 'kyazdani42/nvim-web-devicons', config = require('asterismono.plugin-rc.nvim-web-devicons-rc') } -- File icons
+  use { 'akinsho/nvim-bufferline.lua', config = function() require('asterismono.plugin-rc.bufferline-rc') end } -- Tabline
+  use { 'kyazdani42/nvim-web-devicons', config = function() require('asterismono.plugin-rc.nvim-web-devicons-rc') end } -- File icons
   use 'onsails/lspkind.nvim' -- Auto complete icons
-  use { 'lewis6991/gitsigns.nvim', config = require('asterismono.plugin-rc.gitsigns-rc') } -- Git status line
-  use { 'norcalli/nvim-colorizer.lua', config = require('asterismono.plugin-rc.nvim-colorizer-rc') } -- Color code highlighter
-  use { 'nvim-tree/nvim-tree.lua', config = require('asterismono.plugin-rc.nvim-tree-rc') } -- Tree-style file explorer
-  use { 'nvim-lualine/lualine.nvim', config = require('asterismono.plugin-rc.lualine-rc') } -- Status line
-  use { 'kdheepak/lazygit.nvim', config = require('asterismono.plugin-rc.lazygit-rc') } -- Lazygit interface
-  use { 'lukas-reineke/indent-blankline.nvim', config = require('asterismono.plugin-rc.indent-blankline-rc') } -- Indent lines
+  use { 'lewis6991/gitsigns.nvim', config = function() require('asterismono.plugin-rc.gitsigns-rc') end } -- Git status line
+  use { 'norcalli/nvim-colorizer.lua', config = function() require('asterismono.plugin-rc.nvim-colorizer-rc') end } -- Color code highlighter
+  use { 'nvim-tree/nvim-tree.lua', config = function() require('asterismono.plugin-rc.nvim-tree-rc') end } -- Tree-style file explorer
+  use { 'nvim-lualine/lualine.nvim', config = function() require('asterismono.plugin-rc.lualine-rc') end } -- Status line
+  use { 'kdheepak/lazygit.nvim', config = function() require('asterismono.plugin-rc.lazygit-rc') end } -- Lazygit interface
+  use { 'lukas-reineke/indent-blankline.nvim',
+    config = function() require('asterismono.plugin-rc.indent-blankline-rc') end } -- Indent lines
 
   -- Color schemes
   use 'sainnhe/everforest'
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,35 +74,15 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
-  LuaSnip = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/LuaSnip",
-    url = "https://github.com/L3MON4D3/LuaSnip"
-  },
   ReplaceWithRegister = {
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/ReplaceWithRegister",
     url = "https://github.com/vim-scripts/ReplaceWithRegister"
   },
-  ["cmp-nvim-lsp"] = {
+  ["coc.nvim"] = {
     loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
-    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
-  ["cmp-path"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/cmp-path",
-    url = "https://github.com/hrsh7th/cmp-path"
-  },
-  cmp_luasnip = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
-    url = "https://github.com/saadparwaiz1/cmp_luasnip"
-  },
-  ["emmet-ls"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/emmet-ls",
-    url = "https://github.com/aca/emmet-ls"
+    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    url = "https://github.com/neoclide/coc.nvim"
   },
   everforest = {
     loaded = true,
@@ -110,16 +90,37 @@ _G.packer_plugins = {
     url = "https://github.com/sainnhe/everforest"
   },
   ["gitsigns.nvim"] = {
+    config = { "\27LJ\2\nA\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0&asterismono.plugin-rc.gitsigns-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["hop.nvim"] = {
+    config = { "\27LJ\2\n<\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0!asterismono.plugin-rc.hop-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/hop.nvim",
     url = "https://github.com/phaazon/hop.nvim"
   },
+  ["indent-blankline.nvim"] = {
+    config = { "\27LJ\2\nI\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0.asterismono.plugin-rc.indent-blankline-rc\frequire\0" },
+    loaded = true,
+    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
+  },
+  ["iswap.nvim"] = {
+    config = { "\27LJ\2\n>\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0#asterismono.plugin-rc.iswap-rc\frequire\0" },
+    loaded = true,
+    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/iswap.nvim",
+    url = "https://github.com/mizlan/iswap.nvim"
+  },
+  ["key-menu.nvim"] = {
+    config = { "\27LJ\2\nA\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0&asterismono.plugin-rc.key-menu-rc\frequire\0" },
+    loaded = true,
+    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/key-menu.nvim",
+    url = "https://github.com/linty-org/key-menu.nvim"
+  },
   ["lazygit.nvim"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0%asterismono.plugin-rc.lazygit-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/lazygit.nvim",
     url = "https://github.com/kdheepak/lazygit.nvim"
@@ -129,97 +130,72 @@ _G.packer_plugins = {
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/live-server",
     url = "https://github.com/manzeloth/live-server"
   },
-  ["lsp_signature.nvim"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/lsp_signature.nvim",
-    url = "https://github.com/ray-x/lsp_signature.nvim"
-  },
   ["lspkind.nvim"] = {
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
     url = "https://github.com/onsails/lspkind.nvim"
   },
-  ["lspsaga.nvim"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
-    url = "https://github.com/glepnir/lspsaga.nvim"
-  },
   ["lualine.nvim"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0%asterismono.plugin-rc.lualine-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["marks.nvim"] = {
+    config = { "\27LJ\2\nC\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0(asterismono.plugin-rc.marks-nvim-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/marks.nvim",
     url = "https://github.com/chentoast/marks.nvim"
-  },
-  ["mason-lspconfig.nvim"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
-    url = "https://github.com/williamboman/mason-lspconfig.nvim"
-  },
-  ["mason.nvim"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/mason.nvim",
-    url = "https://github.com/williamboman/mason.nvim"
   },
   ["mkdir.nvim"] = {
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/mkdir.nvim",
     url = "https://github.com/jghauser/mkdir.nvim"
   },
-  ["null-ls.nvim"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
-    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
-  },
   ["nvim-autopairs"] = {
+    config = { "\27LJ\2\nD\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0)asterismono.plugin-rc.nvim-autopairs\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-bufferline.lua"] = {
+    config = { "\27LJ\2\nC\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0(asterismono.plugin-rc.bufferline-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-bufferline.lua",
     url = "https://github.com/akinsho/nvim-bufferline.lua"
   },
-  ["nvim-cmp"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-cmp",
-    url = "https://github.com/hrsh7th/nvim-cmp"
-  },
   ["nvim-colorizer.lua"] = {
+    config = { "\27LJ\2\nG\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0,asterismono.plugin-rc.nvim-colorizer-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-cursorline"] = {
+    config = { "\27LJ\2\nH\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0-asterismono.plugin-rc.nvim-cursorline-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-cursorline",
     url = "https://github.com/yamatsum/nvim-cursorline"
   },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
-  },
   ["nvim-neoclip.lua"] = {
+    config = { "\27LJ\2\nE\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0*asterismono.plugin-rc.nvim-neoclip-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-neoclip.lua",
     url = "https://github.com/AckslD/nvim-neoclip.lua"
   },
   ["nvim-tree.lua"] = {
+    config = { "\27LJ\2\nB\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0'asterismono.plugin-rc.nvim-tree-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
+    config = { "\27LJ\2\nH\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0-asterismono.plugin-rc.nvim-treesitter-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
+    config = { "\27LJ\2\nJ\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0/asterismono.plugin-rc.nvim-web-devicons-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
@@ -240,6 +216,7 @@ _G.packer_plugins = {
     url = "https://github.com/wellle/targets.vim"
   },
   ["telescope.nvim"] = {
+    config = { "\27LJ\2\nB\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0'asterismono.plugin-rc.telescope-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
@@ -255,6 +232,7 @@ _G.packer_plugins = {
     url = "https://github.com/amadeus/vim-convert-color-to"
   },
   ["vim-sneak"] = {
+    config = { "\27LJ\2\nB\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0'asterismono.plugin-rc.vim-sneak-rc\frequire\0" },
     loaded = true,
     path = "/var/home/cmiki/.local/share/nvim/site/pack/packer/start/vim-sneak",
     url = "https://github.com/justinmk/vim-sneak"
@@ -277,6 +255,78 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-neoclip.lua
+time([[Config for nvim-neoclip.lua]], true)
+try_loadstring("\27LJ\2\nE\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0*asterismono.plugin-rc.nvim-neoclip-rc\frequire\0", "config", "nvim-neoclip.lua")
+time([[Config for nvim-neoclip.lua]], false)
+-- Config for: marks.nvim
+time([[Config for marks.nvim]], true)
+try_loadstring("\27LJ\2\nC\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0(asterismono.plugin-rc.marks-nvim-rc\frequire\0", "config", "marks.nvim")
+time([[Config for marks.nvim]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\nB\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0'asterismono.plugin-rc.nvim-tree-rc\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\nB\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0'asterismono.plugin-rc.telescope-rc\frequire\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
+-- Config for: nvim-treesitter
+time([[Config for nvim-treesitter]], true)
+try_loadstring("\27LJ\2\nH\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0-asterismono.plugin-rc.nvim-treesitter-rc\frequire\0", "config", "nvim-treesitter")
+time([[Config for nvim-treesitter]], false)
+-- Config for: lualine.nvim
+time([[Config for lualine.nvim]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0%asterismono.plugin-rc.lualine-rc\frequire\0", "config", "lualine.nvim")
+time([[Config for lualine.nvim]], false)
+-- Config for: nvim-bufferline.lua
+time([[Config for nvim-bufferline.lua]], true)
+try_loadstring("\27LJ\2\nC\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0(asterismono.plugin-rc.bufferline-rc\frequire\0", "config", "nvim-bufferline.lua")
+time([[Config for nvim-bufferline.lua]], false)
+-- Config for: lazygit.nvim
+time([[Config for lazygit.nvim]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0%asterismono.plugin-rc.lazygit-rc\frequire\0", "config", "lazygit.nvim")
+time([[Config for lazygit.nvim]], false)
+-- Config for: nvim-web-devicons
+time([[Config for nvim-web-devicons]], true)
+try_loadstring("\27LJ\2\nJ\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0/asterismono.plugin-rc.nvim-web-devicons-rc\frequire\0", "config", "nvim-web-devicons")
+time([[Config for nvim-web-devicons]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\nD\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0)asterismono.plugin-rc.nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
+-- Config for: vim-sneak
+time([[Config for vim-sneak]], true)
+try_loadstring("\27LJ\2\nB\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0'asterismono.plugin-rc.vim-sneak-rc\frequire\0", "config", "vim-sneak")
+time([[Config for vim-sneak]], false)
+-- Config for: gitsigns.nvim
+time([[Config for gitsigns.nvim]], true)
+try_loadstring("\27LJ\2\nA\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0&asterismono.plugin-rc.gitsigns-rc\frequire\0", "config", "gitsigns.nvim")
+time([[Config for gitsigns.nvim]], false)
+-- Config for: indent-blankline.nvim
+time([[Config for indent-blankline.nvim]], true)
+try_loadstring("\27LJ\2\nI\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0.asterismono.plugin-rc.indent-blankline-rc\frequire\0", "config", "indent-blankline.nvim")
+time([[Config for indent-blankline.nvim]], false)
+-- Config for: nvim-colorizer.lua
+time([[Config for nvim-colorizer.lua]], true)
+try_loadstring("\27LJ\2\nG\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0,asterismono.plugin-rc.nvim-colorizer-rc\frequire\0", "config", "nvim-colorizer.lua")
+time([[Config for nvim-colorizer.lua]], false)
+-- Config for: iswap.nvim
+time([[Config for iswap.nvim]], true)
+try_loadstring("\27LJ\2\n>\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0#asterismono.plugin-rc.iswap-rc\frequire\0", "config", "iswap.nvim")
+time([[Config for iswap.nvim]], false)
+-- Config for: hop.nvim
+time([[Config for hop.nvim]], true)
+try_loadstring("\27LJ\2\n<\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0!asterismono.plugin-rc.hop-rc\frequire\0", "config", "hop.nvim")
+time([[Config for hop.nvim]], false)
+-- Config for: key-menu.nvim
+time([[Config for key-menu.nvim]], true)
+try_loadstring("\27LJ\2\nA\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0&asterismono.plugin-rc.key-menu-rc\frequire\0", "config", "key-menu.nvim")
+time([[Config for key-menu.nvim]], false)
+-- Config for: nvim-cursorline
+time([[Config for nvim-cursorline]], true)
+try_loadstring("\27LJ\2\nH\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0-asterismono.plugin-rc.nvim-cursorline-rc\frequire\0", "config", "nvim-cursorline")
+time([[Config for nvim-cursorline]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
