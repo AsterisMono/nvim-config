92350568ac16ff406c82725f4f8a3bcbfef35db9
Chatnoir Miki <cmiki@amono.me>
2022-11-22 14:32:30 +0800

    Ensure Packer

--- init.lua
+++ init.lua
@@ -7,6 +7,19 @@ vim.g.loaded_netrwPlugin = 1
 --   require('asterismono.plugin-rc.lsp_signature-rc')
 -- )
 
-require('asterismono.base')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 require('asterismono.plugins')
+require('asterismono.base')
 require('asterismono.maps')
