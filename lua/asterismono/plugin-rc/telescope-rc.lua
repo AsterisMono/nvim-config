e3d33ad72e827b9ce58e0cdef1740753f795b590
Chatnoir Miki <cmiki@amono.me>
2022-12-01 16:33:43 +0800

    Change telescope mode to normal for some plugins

--- lua/asterismono/plugin-rc/telescope-rc.lua
+++ lua/asterismono/plugin-rc/telescope-rc.lua
@@ -17,8 +17,8 @@ vim.keymap.set('n', '\'\'', '<cmd>Telescope file_browser initial_mode=normal<CR>
 -- require 'key-menu'.set('n', '\'', { desc = 'Telescope' })
 vim.keymap.set('n', '\'f', builtin.find_files, { desc = 'Find files' })
 vim.keymap.set('n', '\'r', builtin.live_grep, { desc = 'Live grep' })
-vim.keymap.set('n', '\'b', builtin.buffers, { desc = 'Buffers' })
-vim.keymap.set('n', '\'y', '<cmd>Telescope neoclip<CR>', { desc = 'Registers' })
+vim.keymap.set('n', '\'b', '<cmd>Telescope buffers initial_mode=normal<CR>', { desc = 'Buffers' })
+vim.keymap.set('n', '\'y', '<cmd>Telescope neoclip initial_mode=normal<CR>', { desc = 'Registers' })
 vim.keymap.set('n', '\'s', '<cmd>Telescope session-lens search_session<CR>', { desc = 'Sessions' })
 vim.keymap.set('n', '<leader>m', builtin.marks, { desc = 'Marks' })
 
