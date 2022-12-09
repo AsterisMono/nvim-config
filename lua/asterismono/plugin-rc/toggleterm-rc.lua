605ca0e062c4001e3385439c19d1755829885666
Chatnoir Miki <cmiki@amono.me>
2022-12-09 11:22:01 +0800

    Add toggleterm keymap

--- lua/asterismono/plugin-rc/toggleterm-rc.lua
+++ lua/asterismono/plugin-rc/toggleterm-rc.lua
@@ -1,3 +1,15 @@
 require("toggleterm").setup()
 
-vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<CR>', { desc = 'Toggle term' })
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<Cmd>ToggleTerm<CR>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+vim.keymap.set('n', '<leader>t', [[<Cmd>ToggleTerm<CR>]], { desc = 'Toggle term' })
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
