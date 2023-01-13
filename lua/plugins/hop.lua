a2640c2ebd702a0da0f5948f1f480eb10c48f401
Chatnoir Miki <cmiki@amono.me>
2023-01-13 15:48:26 +0800

    Fill plugins

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,18 @@
+return {
+  'phaazon/hop.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local hop = require('hop')
+    hop.setup()
+
+    vim.keymap.set('n', '<Space>', '<cmd>HopChar2<CR>')
+
+    local directions = require('hop.hint').HintDirection
+    vim.keymap.set('', 'f', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+    end, { remap = true })
+    vim.keymap.set('', 'F', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+    end, { remap = true })
+  end
+}
--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,5 +1,5 @@
 return {
   'norcalli/nvim-colorizer.lua',
-  event = 'VeryLazy',
+  event = 'BufReadPost',
   config = true
 }
