488fbceba26f87debeaa70bcfefdb1c7ad17c2e6
Chatnoir Miki <cmiki@amono.me>
2023-01-15 12:54:15 +0800

    Use coc to format

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -17,9 +17,6 @@ return {
       enable = true,
       additional_vim_regex_highlighting = false,
     },
-    indent = {
-      enable = true
-    },
     incremental_selection = {
       enable = true,
       keymaps = {
