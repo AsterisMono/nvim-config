d68ecb6deda63c57a4efd2a75a8422e96fc21b42
Chatnoir Miki <cmiki@amono.me>
2023-05-05 20:49:47 +0800

    Initialize lspzero

--- lua/plugins/live-server.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'manzeloth/live-server',
-  keys = {
-    { '<leader>ls', '<cmd>LiveServer start<CR>', { desc = 'Start liveserver' } },
-  }
-}
---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,26 @@
+return {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  dependencies = {
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' }, -- Required
+    {
+      'williamboman/mason.nvim',
+      build = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },     -- Required
+    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+    { 'L3MON4D3/LuaSnip' },     -- Required
+  },
+  config = function()
+    local lsp = require('lsp-zero').preset({})
+    lsp.on_attach(function(client, bufnr)
+      lsp.default_keymaps({ buffer = bufnr })
+    end)
+  end,
+}
