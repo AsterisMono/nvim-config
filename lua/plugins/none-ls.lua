0ffe3bb815159fcdb6eec76b5fac920208977115
Chatnoir Miki <cmiki@amono.me>
2023-11-29 17:09:06 +0800

    add none-ls

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,13 @@
+return {
+  'nvimtools/none-ls.nvim',
+  event = 'VeryLazy',
+  config = function ()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.prettierd
+      }
+    })
+  end
+}
