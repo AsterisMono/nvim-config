defe092e6d10d7ecf413d3ed44272bbfb1c73ebd
Chatnoir Miki <cmiki@amono.me>
2023-12-01 10:22:00 +0800

    Add copilot plugin

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  'github/copilot.vim',
+  config = function()
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+      expr = true,
+      replace_keycodes = false
+    })
+    vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
+    vim.keymap.set('i', '<C-H>', '<Plug>(copilot-suggest)')
+    vim.g.copilot_no_tab_map = true
+  end
+}
