3ab390de0d036880ebd26fc3158de90aae748b1a
Chatnoir Miki <cmiki@amono.me>
2023-12-01 10:25:40 +0800

    Add telescope shortcut for fzf in current file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
   keys = {
     { '\'f', '<cmd>Telescope find_files<CR>',                 desc = 'Find files' },
     { '\'r', '<cmd>Telescope live_grep<CR>',                  desc = 'Live grep' },
+    { '\'R', '<cmd>Telescope current_buffer_fuzzy_find<CR>',  desc = 'Grep in current file' },
     { '\'b', '<cmd>Telescope buffers<CR>',                    desc = 'Buffers' },
     { '\'e', '<cmd>Telescope resume initial_mode=normal<CR>', desc = 'Resume' },
   },
