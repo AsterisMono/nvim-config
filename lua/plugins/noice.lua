9af950ef1a3c00e18ed8611f7afd70399f545fef
Chatnoir Miki <cmiki@amono.me>
2024-04-15 14:57:03 +0800

    Twilight & fix dependency

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,5 +1,8 @@
 return {
   'folke/noice.nvim',
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
   event = 'VeryLazy',
   opts = {
     cmdline = {
---
+++ lua/plugins/twilight-nvim.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/twilight.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    dimming = {
+      color = { "#6D718C" },
+      alpha = 1,
+    }
+  }
+}
