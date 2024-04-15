011a4266c217af374152207b71ceb2d7eb3a713a
Chatnoir Miki <cmiki@amono.me>
2024-04-15 09:42:42 +0800

    Use esc for exiting bufterm

--- lua/plugins/bufterm.lua
+++ lua/plugins/bufterm.lua
@@ -2,7 +2,7 @@ return {
   'boltlessengineer/bufterm.nvim',
   config = function()
     require('bufterm').setup()
-    vim.keymap.set('t', '<C-q>', '<C-\\><C-n>')
+    vim.keymap.set('t', '<C-[>', '<C-\\><C-n>')
   end,
   keys = {
     { '<leader>t', '<cmd>BufTermEnter<CR>', desc = 'Open buffer terminal' },
