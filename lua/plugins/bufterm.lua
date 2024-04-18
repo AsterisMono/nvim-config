77016a86bedf58c467c39edacc46fcc9ffd4c461
Chatnoir Miki <cmiki@amono.me>
2024-04-18 14:09:11 +0800

    Use floating terminal

--- lua/plugins/bufterm.lua
+++ lua/plugins/bufterm.lua
@@ -6,5 +6,25 @@ return {
   end,
   keys = {
     { '<leader>t', '<cmd>BufTermEnter<CR>', desc = 'Open buffer terminal' },
+    {
+      '<C-t>',
+      function()
+        local term        = require('bufterm.terminal')
+        local ui          = require('bufterm.ui')
+        local recent_term = term.get_recent_term()
+        -- if recent_term is nil, spawn a new terminal
+        if not recent_term then
+          local Terminal = term.Terminal
+          local new_term = Terminal:new({
+            buflisted = false,
+          })
+          new_term:spawn()
+          recent_term = term.get_recent_term()
+        end
+        ui.toggle_float(recent_term.bufnr)
+      end,
+      desc = 'Open buffer terminal',
+      mode = { "n", "t" }
+    },
   }
 }
