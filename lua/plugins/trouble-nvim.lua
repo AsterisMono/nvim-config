80c0bdd4366611c48c9c9b8e3dfb7b67e570c0c1
Chatnoir Miki <cmiki@amono.me>
2024-06-14 15:29:02 +0800

    Update

--- lua/plugins/trouble-nvim.lua
+++ lua/plugins/trouble-nvim.lua
@@ -1,16 +1,32 @@
 return {
   "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
-    { "<leader>xx", function() require("trouble").open() end, desc = "Trouble" },
-    { "<leader>xw", function() require("trouble").open("workspace_diagnostics") end, desc = "Workspace Diagnostics" },
-    { "<leader>xd", function() require("trouble").open("document_diagnostics") end, desc = "Document Diagnostics" },
-    { "<leader>xq", function() require("trouble").open("quickfix") end, desc = "Quickfix" },
-    { "<leader>xl", function() require("trouble").open("loclist") end, desc = "Loc List" },
-    { "gR", function() require("trouble").open("lsp_references") end, desc = "LSP References" },
-  },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xb",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "gR",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xl",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xq",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
 }
