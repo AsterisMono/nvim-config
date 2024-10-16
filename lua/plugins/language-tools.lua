bdd678d23109af4c69f2cc9a04dfbdde44096a2a
Chatnoir Miki <cmiki@amono.me>
2024-10-16 19:14:16 +0800

    feat: lazy load optimization

--- lua/plugins/language-tools.lua
+++ lua/plugins/language-tools.lua
@@ -1,38 +1,42 @@
 return {
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      on_attach = function(client, bufnr)
-        -- Disable tsserver formatting, we're rolling biome/prettier
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-      end,
-      settings = {
-        tsserver_locale = "zh_cn"
-      }
-    },
-  },
-  -- Conform.nvim for code formatting
-  {
-    'stevearc/conform.nvim',
-    ---@module "conform"
-    ---@type conform.setupOpts
-    opts = {
-      formatters_by_ft = {
-        typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
-        javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
-        lua = { "stylua" },
-        nix = { "nixpkgs_fmt" },
-      }
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function(args)
-          require("conform").format({ bufnr = args.buf })
-        end,
-      })
-    end
-  }
+	{
+		"pmizio/typescript-tools.nvim",
+		ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			on_attach = function(client, bufnr)
+				-- Disable tsserver formatting, we're rolling biome/prettier
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end,
+			settings = {
+				tsserver_locale = "zh_cn",
+			},
+		},
+	},
+	-- Conform.nvim for code formatting
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		---@module "conform"
+		---@type conform.setupOpts
+		opts = {
+			formatters_by_ft = {
+				typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+				javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+				lua = { "stylua" },
+				nix = { "nixpkgs_fmt" },
+			},
+			format_on_save = { timeout_ms = 500 },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/text-edit-enhancements.lua
+++ lua/plugins/text-edit-enhancements.lua
@@ -1,31 +1,29 @@
 return {
-  {
-    'tpope/vim-commentary',
-    event = 'VeryLazy'
-  },
-  {
-    'tpope/vim-surround',
-    event = 'VeryLazy'
-  },
-  {
-    'kana/vim-textobj-entire',
-    event = 'VeryLazy',
-    dependencies = {
-      'kana/vim-textobj-user'
-    },
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    init = function()
-      -- Insert `(` after select function or method item
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      local cmp = require('cmp')
-      cmp.event:on(
-        'confirm_done',
-        cmp_autopairs.on_confirm_done()
-      )
-    end,
-    opts = {}
-  }
+	{
+		"tpope/vim-commentary",
+		event = "InsertEnter",
+	},
+	{
+		"tpope/vim-surround",
+		event = "InsertEnter",
+	},
+	{
+		"kana/vim-textobj-entire",
+		event = "InsertEnter",
+		dependencies = {
+			"kana/vim-textobj-user",
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+
+			-- Insert `(` after select function or method item
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
