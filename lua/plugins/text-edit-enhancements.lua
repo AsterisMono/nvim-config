1b8c71b1bd126043bfdd406dc2c5556619ea17b4
Chatnoir Miki <cmiki@amono.me>
2024-10-16 21:07:06 +0800

    feat(text-edit): add plugins

--- lua/plugins/text-edit-enhancements.lua
+++ lua/plugins/text-edit-enhancements.lua
@@ -26,4 +26,68 @@ return {
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				char = {
+					jump_labels = true,
+				},
+			},
+		},
+    -- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		},
+	},
+	{
+		"kevinhwang91/nvim-fundo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			lazy = true,
+		},
+		build = function()
+			require("fundo").install()
+		end,
+		config = true,
+		event = "VeryLazy",
+	},
+	{
+		"vim-scripts/ReplaceWithRegister",
+		event = "VeryLazy",
+	},
+	{
+		"wellle/targets.vim",
+		event = "VeryLazy",
+	},
+	{
+		"gbprod/yanky.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("yanky").setup({
+				highlight = {
+					on_put = false,
+					on_yank = false,
+				},
+			})
+			vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
+
+			vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+			vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+			vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+			vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+
+			vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
+			vim.keymap.set("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)")
+			vim.keymap.set("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)")
+			vim.keymap.set("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)")
+
+			vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+			vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
+		end,
+	},
 }
