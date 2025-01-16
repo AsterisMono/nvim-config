1185da07c0e05084a4aa7cde882b5d482acc27e8
Chatnoir Miki <cmiki@amono.me>
2025-01-16 16:31:43 +0800

    feat: revive surround

--- lua/plugins/text-edit-enhancements.lua
+++ lua/plugins/text-edit-enhancements.lua
@@ -4,8 +4,10 @@ return {
 		event = "InsertEnter",
 	},
 	{
-		"tpope/vim-surround",
-		event = "InsertEnter",
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		opts = {},
 	},
 	{
 		"kana/vim-textobj-entire",
@@ -39,7 +41,7 @@ return {
 		},
     -- stylua: ignore
 		keys = {
-			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "s", mode = "n", function() require("flash").jump() end, desc = "Flash" },
 			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
       { "<leader>ls", function() require("flash").treesitter() end, desc = "Flash Treesitter" },
