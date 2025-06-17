560012affefb1b07ec926fb872f420e2df74c7c1
Noa Virellia <cmiki@amono.me>
2025-06-17 21:11:46 +0800

    feat: remove zellij

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -189,17 +189,4 @@ return {
 			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 		end,
 	},
-	-- Zellij integration
-	{
-		"swaits/zellij-nav.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		keys = {
-			{ "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
-			{ "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-			{ "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-			{ "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
-		},
-		opts = {},
-	},
 }
