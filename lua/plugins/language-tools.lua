8b75ddec2ce389af0816568c2e1989459561c141
Noa Virellia <cmiki@amono.me>
2025-06-17 21:27:01 +0800

    feat: remove incremental selection

--- lua/plugins/language-tools.lua
+++ lua/plugins/language-tools.lua
@@ -69,14 +69,6 @@ return {
 					enable = true,
 					additional_vim_regex_highlighting = false,
 				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<CR>",
-						node_incremental = "<CR>",
-						node_decremental = "<BS>",
-					},
-				},
 				textobjects = {
 					select = {
 						enable = true,
