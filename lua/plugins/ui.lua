bf62a1ef097c41394e26cc6e970a0bdd0011e375
Noa Virellia <cmiki@amono.me>
2025-06-18 13:55:37 +0800

    fix: remove unused menu

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -78,7 +78,6 @@ return {
 			wk.add({ "<leader>l", group = "LSP Actions" })
 			wk.add({ "<leader>g", group = "Git" })
 			wk.add({ "<leader>s", group = "Sessions" })
-			wk.add({ "<leader>b", group = "Buffers" })
 			wk.add({ "'g", group = "Telescope Git" })
 			wk.add({ "'l", group = "Telescope LSP" })
 		end,
