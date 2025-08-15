f139431e625e68c1fc908256c4774a59d337d7d9
Noa Virellia <cmiki@amono.me>
2025-08-15 22:05:49 +0800

    feat: direction aware splitting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,11 +13,23 @@ keymap.set("n", "<leader>h", "<cmd>noh<CR>", { desc = "Clear highlight" })
 keymap.set("n", "<C-s>", "<cmd>write<CR>", opts)
 keymap.set("n", "<C-x>", safe_exit, opts)
 
+local direction_aware_split = function()
+	local cur_win_width = vim.api.nvim_win_get_width(0)
+	local cur_win_height = vim.api.nvim_win_get_height(0)
+	-- ooh magic number!
+	if cur_win_width >= cur_win_height * 3 then
+		vim.cmd("vsp")
+	else
+		vim.cmd("sp")
+	end
+end
+
 -- Better window navigation
 keymap.set("n", "<C-h>", "<C-w>h", opts)
 keymap.set("n", "<C-j>", "<C-w>j", opts)
 keymap.set("n", "<C-k>", "<C-w>k", opts)
 keymap.set("n", "<C-l>", "<C-w>l", opts)
+keymap.set("n", "<C-q>", direction_aware_split, opts)
 
 -- Resize windows with arrows
 -- delta: 2 lines
