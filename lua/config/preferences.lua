1786e51f9e6723f6a2f02b0609244913adad697b
Noa Virellia <cmiki@amono.me>
2025-08-22 15:27:50 +0800

    feat(neovide): disable scroll animation

--- lua/config/preferences.lua
+++ lua/config/preferences.lua
@@ -119,5 +119,5 @@ if vim.g.neovide then
 
 	vim.g.neovide_cursor_animation_length = 0
 	vim.g.neovide_cursor_short_animation_length = 0
-	vim.g.neovide_scroll_animation_length = 0.1
+	vim.g.neovide_scroll_animation_length = 0
 end
