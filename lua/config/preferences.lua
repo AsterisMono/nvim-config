08123820ea779647e78f8ba44d90f83adce0f295
Noa Virellia <cmiki@amono.me>
2025-08-28 15:50:50 +0800

    chore: merge commit

--- lua/config/preferences.lua
+++ lua/config/preferences.lua
@@ -117,7 +117,11 @@ if vim.g.neovide then
 		callback = set_ime,
 	})
 
-	vim.g.neovide_cursor_animation_length = 0
-	vim.g.neovide_cursor_short_animation_length = 0
-	vim.g.neovide_scroll_animation_length = 0
+	vim.g.neovide_position_animation_length = 0
+	vim.g.neovide_cursor_animation_length = 0.00
+	vim.g.neovide_cursor_trail_size = 0
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_scroll_animation_far_lines = 0
+	vim.g.neovide_scroll_animation_length = 0.00
 end
