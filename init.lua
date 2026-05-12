8e38ca9faece0941b9fba7392af1bc19b8155aac
Noa Virellia <noa@requiem.garden>
2026-05-13 00:14:03 +0800

    feat: add stylua config and reformat

--- init.lua
+++ init.lua
@@ -15,22 +15,22 @@ vim.opt.tabstop = 2 -- Number of spaces tabs count for
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.diagnostic.config({
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "",
-			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.INFO] = "",
-			[vim.diagnostic.severity.HINT] = "",
-		},
-	},
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
+    },
+  },
 })
 
 -- mini.nvim
 -- UI and basics
 require("mini.icons").setup()
 require("mini.basics").setup({
-	options = { extra_ui = true },
-	mappings = { windows = true },
+  options = { extra_ui = true },
+  mappings = { windows = true },
 })
 require("mini.notify").setup()
 require("mini.cmdline").setup()
@@ -39,7 +39,7 @@ require("mini.starter").setup()
 require("mini.tabline").setup()
 require("mini.trailspace").setup()
 require("mini.indentscope").setup({
-	draw = { delay = 0 },
+  draw = { delay = 0 },
 })
 require("mini.extra").setup()
 require("mini.diff").setup()
@@ -47,37 +47,21 @@ require("mini.diff").setup()
 -- Buffer management
 -- [b, ]b for buffer switching
 require("mini.bufremove").setup()
-vim.keymap.set("n", "X", function()
-	MiniBufremove.delete()
-end, opts)
+vim.keymap.set("n", "X", function() MiniBufremove.delete() end, opts)
 
 -- Picker (Telescope alternative)
 require("mini.pick").setup()
-vim.keymap.set("n", "<leader>f", function()
-	MiniPick.builtin.files()
-end, opts)
-vim.keymap.set("n", "<leader>r", function()
-	MiniPick.builtin.grep_live()
-end, opts)
-vim.keymap.set("n", "<leader>b", function()
-	MiniPick.builtin.buffers()
-end, opts)
-vim.keymap.set("n", "<leader>R", function()
-	MiniPick.builtin.resume()
-end, opts)
-vim.keymap.set("n", "<leader>d", function()
-	MiniExtra.pickers.diagnostic()
-end, opts)
-vim.keymap.set("n", "<leader>gh", function()
-	MiniExtra.pickers.git_hunks()
-end, opts)
+vim.keymap.set("n", "<leader>f", function() MiniPick.builtin.files() end, opts)
+vim.keymap.set("n", "<leader>r", function() MiniPick.builtin.grep_live() end, opts)
+vim.keymap.set("n", "<leader>b", function() MiniPick.builtin.buffers() end, opts)
+vim.keymap.set("n", "<leader>R", function() MiniPick.builtin.resume() end, opts)
+vim.keymap.set("n", "<leader>d", function() MiniExtra.pickers.diagnostic() end, opts)
+vim.keymap.set("n", "<leader>gh", function() MiniExtra.pickers.git_hunks() end, opts)
 
 -- File explorer
 require("mini.files").setup()
 local minifiles_toggle = function(...)
-	if not MiniFiles.close() then
-		MiniFiles.open(...)
-	end
+  if not MiniFiles.close() then MiniFiles.open(...) end
 end
 vim.keymap.set("n", "<leader>F", minifiles_toggle, opts)
 
@@ -96,27 +80,17 @@ require("mini.move").setup()
 
 -- Session management
 require("mini.sessions").setup({
-	autoread = true,
+  autoread = true,
 })
-vim.keymap.set("n", "<leader>sw", function()
-	MiniSessions.write()
-end, opts)
-vim.keymap.set("n", "<leader>ss", function()
-	MiniSessions.select()
-end, opts)
-vim.keymap.set("n", "<leader>sR", function()
-	MiniSessions.restart()
-end, opts)
+vim.keymap.set("n", "<leader>sw", function() MiniSessions.write() end, opts)
+vim.keymap.set("n", "<leader>ss", function() MiniSessions.select() end, opts)
+vim.keymap.set("n", "<leader>sR", function() MiniSessions.restart() end, opts)
 
 -- Flash.nvim
 vim.pack.add({ "https://github.com/folke/flash.nvim" })
 require("flash").setup({ modes = { char = { jumplabels = true } } })
-vim.keymap.set("n", "s", function()
-	require("flash").jump()
-end, opts)
-vim.keymap.set("o", "r", function()
-	require("flash").remote()
-end, opts)
+vim.keymap.set("n", "s", function() require("flash").jump() end, opts)
+vim.keymap.set("o", "r", function() require("flash").remote() end, opts)
 
 -- LSP
 vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
@@ -125,23 +99,23 @@ vim.lsp.enable({ "lua_ls", "nixd", "rust_analyzer", "gopls", "html", "hls" })
 -- Treesitter
 vim.pack.add({ "https://github.com/romus204/tree-sitter-manager.nvim" })
 require("tree-sitter-manager").setup({
-	ensure_installed = { "lua", "nix", "rust", "go", "html", "haskell" },
+  ensure_installed = { "lua", "nix", "rust", "go", "html", "haskell" },
 })
 
 -- Conform.nvim (Formatter)
 vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		nix = { "nixfmt" },
-		rust = { "rustfmt" },
-		go = { "gofmt" },
-		html = { "prettier" },
-		haskell = { "ormolu" },
-		yaml = { "yamlfmt" },
-		json = { "prettier" },
-	},
-	format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    nix = { "nixfmt" },
+    rust = { "rustfmt" },
+    go = { "gofmt" },
+    html = { "prettier" },
+    haskell = { "ormolu" },
+    yaml = { "yamlfmt" },
+    json = { "prettier" },
+  },
+  format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 })
 
 -- Lazygit Integration
